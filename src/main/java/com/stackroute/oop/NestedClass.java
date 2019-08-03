be97c3de7fc09468b361d4e7638a1754d3a85d5e
nishi.singh <nishi.singh@cgi.com>
2019-08-03 17:16:54 +0530

    nested class completed both static and non static nested class

---
+++ src/main/java/com/stackroute/oop/NestedClass.java
@@ -0,0 +1,35 @@
+package com.stackroute.oop;
+
+
+class OterClass {
+    //static member
+    static int outer_z = 10;
+    //non-static memeber
+    int outer_y = 20;
+    //private static member
+    private static int outer_x = 50;
+
+    static class StaticOuterClass {
+        void display() {
+            System.out.println(OterClass.outer_z);
+
+            System.out.println(outer_x);
+            //but we can not access non static member
+            //it will give compile time error
+            //System.out.println(outer_y);
+
+        }
+
+
+    }
+}
+
+
+public class NestedClass {
+    public static void main(String[] args) {
+        OterClass.StaticOuterClass object = new OterClass.StaticOuterClass();
+        object.display();
+        OuterClass outerObject=new OuterClass();
+        OuterClass.InnerClass innerClass=outerObject.newInnerClass();
+    }
+}
